a9136a69d572c726ca4d7c2a0788fc69ebab0cfc
Melchor Varela <37381609+EA4FRB@users.noreply.github.com>
2020-05-13 15:37:01 +0200

    Create commondefs.py

---
+++ src/commondefs.py
@@ -0,0 +1,44 @@
+# ---------------------------------------------------------
+"""
+  This file is a part of the "Acoustic Emanations Tool" software
+
+  MIT License
+
+  @author Copyright (c) 2020 Melchor Varela - EA4FRB
+
+  Permission is hereby granted, free of charge, to any person obtaining a copy
+  of this software and associated documentation files (the "Software"), to deal
+  in the Software without restriction, including without limitation the rights
+  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+  copies of the Software, and to permit persons to whom the Software is
+  furnished to do so, subject to the following conditions:
+
+  The above copyright notice and this permission notice shall be included in all
+  copies or substantial portions of the Software.
+
+  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+  SOFTWARE.
+"""
+# ---------------------------------------------------------
+
+
+ORGANIZATION_NAME = 'Melchor Varela - EA4FRB'
+ORGANIZATION_DOMAIN = 'sark110.com'
+APPLICATION_NAME = 'Acoustic Emanations Tool'
+COPYRIGHT_DATE = "© 2020,"
+
+PATH_TRAIN_DEF = '../train'
+SUBPATH_WAV = '/wav/'
+SUBPATH_META = '/metadata/'
+SUBPATH_MODELS = '/saved_models/'
+DEF_KEY_NAME = 'key0'
+METADATA_FILENAME = "train_data.csv"
+CHECKPOINT_FILENAME = 'weights.best.basic_mlp.hdf5'
+FEATURES_FILENAME = 'features.csv'
+
+AUD_TRIM_THRESHOLD = 15
